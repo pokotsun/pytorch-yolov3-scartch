3f91d79090d4ef64d2d82f34046f9107fd24338a
pokotsun <gorigolila@gmail.com>
2019-07-12 00:30:31 +0900

    create detect_video arg_parser function

---
+++ detect_video.py
@@ -0,0 +1,45 @@
+from __future__ import division
+import time
+import torch
+import torch.nn as nn
+from torch.autogred import Variable
+import numpy as np
+import cv2
+from util import *
+from darknet import Darknet
+
+import pandas as pd
+import radnom
+import pickle as pkl
+import argparse
+
+def arg_parse():
+    """
+    Parse arguments to the detect module 
+    """
+
+    parser = argparse.ArgumentParser(description='YOLO v3 Video Detection')
+    parser.add_argument("--video", dest='video',
+            help="Video to run detection upon", default="video.avi", type=str)
+    parser.add_argument("--dataset", dest="dataset",
+            help="Dataset on which the network has been trained", default="pascal")
+    parser.add_argument("--confidence", dest="confidence",
+            help="Object Confidence to filter predictions", default=0.5, type=float)
+    parser.add_argument("--nms_thresh", dest="nms_thresh",
+            help="NMS Threshhold", default=0.4, type=float)
+    parser.add_argument("--cfg", dest="cfgfile",
+            help="Config file", default="cfg/yolov3.cfg", type=str)
+    parser.add_argument("--reso", dest="reso",
+            help="Input resolution of the network. Increase to increase accuracy. Decrease to increase learning speed",
+            default=416, type=int)
+    return parser.parse_args()
+
+def main():
+    args = arg_parse()
+    confidence = args.confidence
+    nms_thresh = args.nms_thresh
+
+if __name__ == '__main__':
+    main()
+
+
