f17c568eb241e4031ff1537c3c63ff84698817ec
pokotsun <gorigolila@gmail.com>
2020-06-05 18:59:05 +0900

    complete to create detect_video.py

--- detect_video.py
+++ detect_video.py
@@ -9,6 +9,7 @@ from util import *
 from darknet import Darknet
 
 import pandas as pd
+import colorsys
 import random
 import pickle as pkl
 import argparse
@@ -41,8 +42,8 @@ def main():
     start = 0
     CUDA = torch.cuda.is_available()
     
-    num_classes = 80
     classes = load_classes("data/coco.names")
+    num_classes = len(classes)
 
     # Set up the neural network
     print("Loading network.....")
@@ -67,7 +68,7 @@ def main():
         c2 = tuple(x[3:5].int())
         cls = int(x[-1])
         label = "{0}".format(classes[cls])
-        cv2.rectangle(img, c1, c2, color, 1)
+        cv2.rectangle(img, c1, c2, color, 4)
         t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1, 1)[0]
         c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
         cv2.rectangle(img, c1, c2, color, -1)
@@ -79,18 +80,48 @@ def main():
 
     frames = 0
     start = time.time()
+    hsv_tuples = [(x / num_classes, 1., 1.) for x in range(num_classes)]
+    colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+    colors = list(map(lambda x: (int(x[0] * 200), int(x[1] * 200), int(x[2] * 200)), colors))
+    np.random.seed(10000)
+    np.random.shuffle(colors)
+    np.random.seed(None) # reset seed to default.
     
     while cap.isOpened():
         ret, frame = cap.read()
 
         if ret:
-            # img = prep_image(frame, inp_dim)
-            # im_dim = frame.shape[1], frame.shape[0]
-            # im_dim = torch.FloatTensor(im_dim).repeat(1, 2)
+            frame = cv2.resize(frame, dsize=(1280, 960))
+            img = prep_image(frame, inp_dim)
+            print(f"IMG_SHAPE: {img.shape}")
+            im_dim = frame.shape[1], frame.shape[0]
+            im_dim = torch.FloatTensor(im_dim).repeat(1, 2)
 
-            # if CUDA:
-            #     im_dim = im_dim.cuda()
-            #     img = img.cuda()
+            if CUDA:
+                im_dim = im_dim.cuda()
+                img = img.cuda()
+
+            with torch.no_grad():
+                outputs = model(Variable(img, volatile=True), CUDA)
+            outputs = write_results(outputs, confidence, num_classes, nms_conf=nms_thresh)
+
+            if outputs != None:
+                im_dim = im_dim.repeat(outputs.size(0), 1)
+                scaling_factor = torch.min(inp_dim/im_dim, 1)[0].view(-1, 1)
+
+                outputs[:, [1,3]] -= (inp_dim - scaling_factor * im_dim[:, 0].view(-1,1)) / 2
+                outputs[:, [2,4]] -= (inp_dim - scaling_factor * im_dim[:, 1].view(-1,1)) / 2
+
+                outputs[:, 1:5] /= scaling_factor
+
+                for i in range(outputs.shape[0]):
+                    outputs[i, [1,3]] = torch.clamp(outputs[i, [1,3]], 0.0, im_dim[i, 0])
+                    outputs[i, [2,4]] = torch.clamp(outputs[i, [2,4]], 0.0, im_dim[i, 1])
+
+                for output in outputs:
+                    color = colors[int(output[-1])]
+                    write(output, frame, color)
+
             cv2.imshow("frame", frame)
             key = cv2.waitKey(1)
             if key & 0xFF == ord('q'):
